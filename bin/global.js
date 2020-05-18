dc2d56a5cfdb291df071cce8cc49d3049b7f6ee3
turinglabsorg <turinglabs@criptext.com>
2020-05-18 12:19:09 +0200

    started publishing scripts

--- bin/global.js
+++ bin/global.js
@@ -4,6 +4,7 @@ var ScryptaBVC = require('../lib/index.js')
 
 var args = process.argv.splice(process.execArgv.length + 2);
 var command = args[0];
+const console = require('better-console')
 
 switch(command){
     case "use":
@@ -25,6 +26,15 @@ switch(command){
         }
     break;
 
+    case "init":
+        if(args[1] !== undefined){
+            let folder = args[1]
+            ScryptaBVC.initfolder(folder)
+        }else{
+            ScryptaBVC.initfolder('./')
+        }
+    break;
+
     case "hash":
         if(args[1] !== undefined){
             let folder = args[1]
@@ -34,12 +44,22 @@ switch(command){
         }
     break;
 
-    case "init":
-        if(args[1] !== undefined){
+    case "publish":
+        if(args[1] !== undefined && args[2] !== undefined){
             let folder = args[1]
-            ScryptaBVC.initfolder(folder)
+            if(args[2] !== undefined){
+                let passphrase = args[2]
+                ScryptaBVC.publishfolder(folder, passphrase)
+            }else{
+                console.error('Passphrase not defined, use the command like `scrypta-bvc Folder Passphrase`')
+            }
         }else{
-            ScryptaBVC.initfolder('./')
+            if(args[1] !== undefined){
+                let passphrase = args[1]
+                ScryptaBVC.publishfolder('./', passphrase)
+            }else{
+                console.error('Passphrase not defined, use the command like `scrypta-bvc Passphrase`')
+            }
         }
     break;
 
